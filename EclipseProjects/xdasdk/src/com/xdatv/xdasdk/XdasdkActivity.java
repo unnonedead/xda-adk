8f3d24202f191f250fe6aa7c1a5b206c6091f785
adamoutler <adamoutler@gmail.com>
2012-04-29 23:00:58 +0000

    Checking in changes after XDATV Creating An Android App part 2

---
+++ EclipseProjects/xdasdk/src/com/xdatv/xdasdk/XdasdkActivity.java
@@ -0,0 +1,61 @@
+package com.xdatv.xdasdk;
+
+/*
+ * Copyright (c) 2012 XDATV
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights 
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ * copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class XdasdkActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        final Button button1 = (Button) findViewById(R.id.button1);
+        
+        button1.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				String command[]={"su", "-c",  "ls /data"};
+				Shell shell = new Shell();
+				String text = shell.sendShellCommand(command);
+				setNewTextInTextView(text);
+
+			}
+		});
+        
+    }
+    public void setNewTextInTextView(String text){
+    	TextView tv= new TextView(this);
+    	tv.setText(text);
+    	setContentView(tv);
+    	
+    	
+    }
+    
+}
